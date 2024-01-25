220a1e916ba64d102eb2d6f46281e6c4d01da746
Marc Zhang <sjetqd@gmail.com>
2024-01-25 13:06:12 -0700

    department added

---
+++ pages/departments.vue
@@ -0,0 +1,23 @@
+<script setup>
+const {data} = await useFetch('/api/departments', {
+  headers: useRequestHeaders(['cookie']),
+});
+const departments = data.value.departments;
+</script>
+<template>
+  <main>
+    <header>
+      <h1>Departments</h1>
+    </header>
+    <div>
+      <ul>
+        <li v-for="department in departments" :key="department.DEPARTMENT_ID">
+        <h2>Department Name: {{department.DEPARTMENT_NAME}}</h2>
+        <p>Department ID: {{ department.DEPARTMENT_ID }}</p>
+        <p>Manager ID: {{ department.MANAGER_ID }}</p>
+      </li>
+      </ul>
+    </div>
+  </main>
+
+</template>
--- server/api/departments.js
+++ server/api/departments.js
@@ -3,6 +3,6 @@ import { serverSupabaseClient } from "#supabase/server";
 export default eventHandler(async (event) => {
   const client = await serverSupabaseClient(event);
 
-  const { data, error } = await client.from('Departments').select();
+  const { data } = await client.from('Departments').select();
   return { departments: data };
 });
